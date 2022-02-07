ebb903c01582214411416d9df18ae422e1c040bf
Sean Hagen <sean.r.hagen@gmail.com>
2022-02-06 23:04:17 -0600

    Added twitter scraper

--- smolDict.py
+++ smolDict.py
@@ -24,14 +24,26 @@ def pickEveryOther():
     with open("./json/outputMythSmall.json", "w") as fl:
         json.dump(finalDict, fl)
 
+def list_to_dict():
+    jsn_list = []
+    jsn_rtn = {}
+    with open("json\\final_results.json") as fl:
+        jsn_list = json.load(fl)
+    for entry in jsn_list:
+        jsn_rtn[entry["wiki_url"]] = "Desc Needed"
+    with open("json\\final_results_dict.json", 'w') as fl:
+        json.dump(jsn_rtn, fl)
+
 def mergeDict():
     finalDict = {}
-    with open("./json/cat.json") as fl:
+    with open("json\\final_results_dict.json") as fl:
         jsn_fl = json.load(fl)
-        with open("./json/outputCatSmall.json") as fl2:
+        with open("json\\unusual.json") as fl2:
             jsn_fl_2 = json.load(fl2)
+            print(f"Original_Length: {len(jsn_fl_2)}")
             finalDict = {**jsn_fl_2, **jsn_fl}
-            with open("./json/cat.json", "w") as fl:
+            with open("json\\unusual.json", "w") as fl:
+                print(f"Final length: {len(finalDict)}")
                 json.dump(finalDict, fl)
 
 if __name__ == "__main__":
